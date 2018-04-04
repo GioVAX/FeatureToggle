33fc7c9dbe35b71178d0f9a453b89e03f7461755
Giovanni Pezzino <g.pezzino@elsevier.com>
2018-04-04 14:41:15 +0100

    Added Functional Tests project

---
+++ FeatureToggle.Web.FunctionalTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FeatureToggle.Web.FunctionalTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FeatureToggle.Web.FunctionalTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("59519556-b996-4416-b2d5-f8a94b9b3347")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
