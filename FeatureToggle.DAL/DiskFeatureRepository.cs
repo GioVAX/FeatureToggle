58bce54947556cbd0c916221647caf54339ae730
Giovanni Pezzino <g.pezzino@elsevier.com>
2018-04-12 22:02:25 +0100

    Repository persistence after Delete

--- FeatureToggle.DAL/DiskFeatureRepository.cs
+++ FeatureToggle.DAL/DiskFeatureRepository.cs
@@ -9,18 +9,28 @@ namespace FeatureToggle.DAL
 {
     public class DiskFeatureRepository : IFeatureRepository
     {
-        List<FeatureConfiguration> _features;
+        private readonly string _filepath;
+        private readonly List<FeatureConfiguration> _features;
 
         public DiskFeatureRepository(string filepath)
         {
-            if (filepath == null || !File.Exists(filepath))
-            {
-                _features = new List<FeatureConfiguration>();
-                return;
-            }
+            _filepath = filepath;
+            _features = LoadConfigurationFile();
+        }
 
-            var json = File.ReadAllText(filepath);
-            _features = JsonConvert.DeserializeObject<List<FeatureConfiguration>>(json);
+        private List<FeatureConfiguration> LoadConfigurationFile()
+        {
+            if (_filepath == null || !File.Exists(_filepath))
+                return new List<FeatureConfiguration>();
+
+            var json = File.ReadAllText(_filepath);
+            return JsonConvert.DeserializeObject<List<FeatureConfiguration>>(json);
+        }
+
+        private void WriteConfigurationFile()
+        {
+            var json = JsonConvert.SerializeObject(_features);
+            File.WriteAllText(_filepath, json);
         }
 
         public IEnumerable<FeatureConfiguration> Select(string pattern)
@@ -35,6 +45,8 @@ namespace FeatureToggle.DAL
         {
             if (_features.RemoveAll(configuration => string.Equals(configuration.Feature, featureName, StringComparison.InvariantCultureIgnoreCase)) == 0)
                 throw new KeyNotFoundException($"Feature <{featureName}> is not configured.");
+
+            WriteConfigurationFile();
         }
     }
 }
