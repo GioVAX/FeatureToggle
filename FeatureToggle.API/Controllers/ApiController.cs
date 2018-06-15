73db16ff0ead33f053b2769d72a65bb5b36ebfeb
Giovanni Pezzino <g.pezzino@elsevier.com>
2018-06-15 16:44:03 +0100

    Fixed project references and tests are passing

---
+++ FeatureToggle.API/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using FeatureToggle.Definitions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeatureToggle.API.Controllers
+{
+    [Produces("application/json")]
+    [Route( "api/Feature/[action]")]
+    public class ApiController : Controller
+    {
+        readonly IFeatureRepository _repository;
+
+        public ApiController(IFeatureRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult GetFeatures(string beginningWith)
+        {
+            var features = _repository.Select(beginningWith);
+
+            return Ok(features);
+        }
+    }
+}
