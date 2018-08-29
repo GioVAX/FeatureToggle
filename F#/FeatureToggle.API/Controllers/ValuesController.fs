b3da80040d7e759eba24df74f8d98a4172261037
Giovanni Pezzino <g.pezzino@elsevier.com>
2018-08-29 18:37:37 +0100

    Added F# initial project

---
+++ F#/FeatureToggle.API/Controllers/ValuesController.fs
@@ -0,0 +1,18 @@
+﻿namespace FeatureToggle.API.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open FeatureToggle.Definitions
+
+[<Produces("application/json")>]
+[<Route( "[action]")>]
+type ValuesController (repository : IFeatureRepository) =
+    inherit Controller()
+
+    [<HttpGet>]
+    member this.GetFeatures( beginningWith : string) =
+        OkObjectResult(repository.Select( beginningWith ))
+        
---
+++ F#/FeatureToggle.API/Startup.fs
@@ -0,0 +1,32 @@
+namespace FeatureToggle.API
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open FeatureToggle.Definitions
+open FeatureToggle.DAL
+
+type Startup private () =
+    new (configuration: IConfiguration) as this =
+        Startup() then
+        this.Configuration <- configuration
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    member this.ConfigureServices(services: IServiceCollection) =
+        // Add framework services.
+        services.AddMvc() |> ignore
+
+        services
+            .AddTransient<IFeatureRepository, DiskFeatureRepository>()
+            .Configure<FeaturesFileConfiguration>(this.Configuration) |> ignore
+                
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    member this.Configure(app: IApplicationBuilder, env: IHostingEnvironment) =
+        app.UseMvc() |> ignore
+
+    member val Configuration : IConfiguration = null with get, set
