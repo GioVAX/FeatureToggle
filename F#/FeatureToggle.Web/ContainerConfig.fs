caaac85d3b0476c16726b26ec994e73df0cb5ba8
Giovanni Pezzino <G.Pezzino@elsevier.com>
2018-12-02 13:06:54 +0000

    Changed lifetime to Scoped

--- F#/FeatureToggle.Web/ContainerConfig.fs
+++ F#/FeatureToggle.Web/ContainerConfig.fs
@@ -10,15 +10,16 @@ let registerAppServices (container:ServiceProvider) (services:IServiceCollection
     let configuration = container.GetService<IConfiguration>()
     
     services
-        .AddTransient<DiskStorage.DiskStorage>(
+        .Configure<FeaturesFileConfiguration>(configuration)
+        .AddScoped<DiskStorage.DiskStorage>(
             fun cnt ->
                 let options = cnt.GetService<IOptions<FeaturesFileConfiguration>>()
                 DiskStorage.createDiskStoreage options.Value
         )
-        .AddTransient<IFeatureRepository>( 
+        .AddScoped<IFeatureRepository>( 
             fun cnt -> 
                 let storage = cnt.GetService<DiskStorage.DiskStorage>()
                 DiskFeatureRepository.createRepository storage
         )
-        .Configure<FeaturesFileConfiguration>(configuration) |> ignore
+        |> ignore
 
